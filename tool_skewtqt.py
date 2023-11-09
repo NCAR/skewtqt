4e9c4ca14f964421907822175663550ca1a853b6
Gary Granger <granger@ucar.edu>
2023-11-09 16:55:12 -0700

    append paths as a list
    otherwise two paths get concatenated when the construction variable has
    not first been set to a list
    use qt tools instead of modules, and remove the qt version, since
    that is specified for the whole project and loaded by the modules.

--- tool_skewtqt.py
+++ tool_skewtqt.py
@@ -5,20 +5,16 @@ upDir   = Dir('./../').abspath
 
 # Define tool
 def skewtqt(env):
-    env.Append(LIBS = [toolname])
-    env.AppendUnique(CPPPATH = upDir)
-    env.AppendUnique(CPPPATH = thisDir)
-    env.AppendUnique(LIBPATH = thisDir)
-    qt_tool = 'qt4'
-    if env.get('QT_VERSION', 4) != 4:
-        qt_tool = 'qt5'
-    env.Require([qt_tool, 'prefixoptions', 'skewt'])
-    env.EnableQtModules(['QtCore','QtGui'])
-    
+    env.Append(LIBS=[toolname])
+    env.AppendUnique(CPPPATH=[upDir])
+    env.AppendUnique(CPPPATH=[thisDir])
+    env.AppendUnique(LIBPATH=[thisDir])
+    env.Require(['prefixoptions', 'skewt', 'qtcore', 'qtgui'])
+
 Export(toolname)
 
 # Build the library
-env = Environment(tools = ['default', 'doxygen', toolname])
+env = Environment(tools=['default', 'doxygen', toolname])
 
 sources = Split("""
    SkewTAdapterQt.cc
